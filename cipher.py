5bbef2ce5ebf993e698917ce40f8efb5a381a273
Keyonna Brown <kejbrown343@gmail.com>
2023-08-01 23:48:03 -0400

    codes

--- cipher.py
+++ cipher.py
@@ -1,21 +1,15 @@
 def caesar_cipher_encrypt(text, shift):
     abc = "abcdefghijklmnopqrstuvwxyz"
     encrypted_text = ""
-    
     for char in text:
-        if char.isalpha():  # Only encrypt alphabetic characters
-            char_lower = char.lower()
-            char_index = abc.find(char_lower)
-            shifted_index = (char_index + shift) % 26
-            encrypted_char = abc[shifted_index]
-            
+        if char.isalpha():
+            shift_amount = shift % 26
+            encrypted_char = chr(((ord(char.lower()) - 97 + shift_amount) % 26) + 97)
             if char.isupper():
-                encrypted_char = encrypted_char.upper()  # Convert back to uppercase if the original character was uppercase
+                encrypted_char = encrypted_char.upper()
         else:
-            encrypted_char = char  # Keep non-alphabetic characters unchanged
-        
+            encrypted_char = char
         encrypted_text += encrypted_char
-    
     return encrypted_text
 
 def main():
@@ -24,12 +18,12 @@ def main():
         if not shift_str.isdecimal():
             print("Invalid input. Please enter a valid number for the shift.")
             return
-        
+
         shift = int(shift_str)
         plain_text = input("Enter the plain text sentence: ").lower()
 
         encrypted_text = caesar_cipher_encrypt(plain_text, shift)
-        print("Encrypted text:", encrypted_text)
+        print("The encrypted sentence is:", encrypted_text)
     except ValueError:
         print("Invalid input. Please enter a valid number for the shift.")
 
