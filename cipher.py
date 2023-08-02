efc12493c6a8e4ed23567922e62b89a03119f014
Keyonna Brown <kejbrown343@gmail.com>
2023-08-02 00:02:58 -0400

    code change

--- cipher.py
+++ cipher.py
@@ -1,31 +1,27 @@
-def caesar_cipher_encrypt(text, shift):
-    abc = "abcdefghijklmnopqrstuvwxyz"
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shift_amount = shift % 26
-            encrypted_char = chr(((ord(char.lower()) - 97 + shift_amount) % 26) + 97)
-            if char.isupper():
-                encrypted_char = encrypted_char.upper()
-        else:
-            encrypted_char = char
-        encrypted_text += encrypted_char
-    return encrypted_text
+abc = "abcdefghijklmnopqrstuvwxyz"
 
-def main():
-    try:
-        shift_str = input("Enter the number of characters for the shift: ")
-        if not shift_str.isdecimal():
-            print("Invalid input. Please enter a valid number for the shift.")
-            return
+shift = int(input("Please enter the number of places to shift: "))
+if not shift:
+    print("Invalid input. Please enter a valid number for the shift.")
+    exit()
 
-        shift = int(shift_str)
-        plain_text = input("Enter the plain text sentence: ").lower()
 
-        encrypted_text = caesar_cipher_encrypt(plain_text, shift)
-        print("The encrypted sentence is:", encrypted_text)
-    except ValueError:
-        print("Invalid input. Please enter a valid number for the shift.")
+sentence = input("Please enter a sentence: ").lower()
 
-if __name__ == "__main__":
-    main()
+encrypted_sentence =  ""
+
+
+for char in sentence:
+    if  char.isalpha():
+         char_index = abc.find(char)
+         shift_index = (char_index + shift) % 26
+         encrypted_char = abc[shift_index]
+         if char.isupper():
+             encrypted_char = encrypted_char.upper()
+         encrypted_sentence += encrypted_char 
+    else:   
+        encrypted_sentence += encrypted_char
+    
+
+
+print("Encrypted Sentence:", encrypted_sentence)
